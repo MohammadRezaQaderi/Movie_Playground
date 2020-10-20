02c324dce66187e1ba7cf2e55a6367d23d8d81aa
MohammadRezaQaderi <you@example.com>
2020-10-20 14:05:59 +0330

    make pic

--- Movie/resources/views/index.blade.php
+++ Movie/resources/views/index.blade.php
@@ -1,7 +1,31 @@
 @extends('layouts.main')
 
 @section('content')
-    <div>
-        index of Movies
+    <div class="container mx-auto px-4 pt-16">
+        <div class="popular-movies">
+            <h2 class="uppercase tarcking-wider text-orange-500 text-lg font-semibold">Popular Movies</h2>
+            <div class="grid grid-col-4 gap-16">
+                <div class="mt-8">
+                    <a href="#">
+                        <img src="/Movie/resources/img/BridsOfPrey.jpg" alt="BridsOfPrey">
+                    </a>
+                </div>
+                <div class="mt-8">
+                    <a href="#">
+                        <img src="/Movie/resources/img/game.jpg" alt="BridsOfPrey">
+                    </a>
+                </div>
+                <div class="mt-8">
+                    <a href="#">
+                        <img src="/Movie/resources/img/game.jpg" alt="BridsOfPrey">
+                    </a>
+                </div>
+                <div class="mt-8">
+                    <a href="#">
+                        <img src="/Movie/resources/img/BridsOfPrey.jpg" alt="BridsOfPrey">
+                    </a>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
--- Movie/resources/views/layouts/main.blade.php
+++ Movie/resources/views/layouts/main.blade.php
@@ -9,11 +9,25 @@
 <body class="font-sans bg-gray-900 text-white">
     <nav class="border-b border-gray-800">
         <div class="container mx-auto flex items-center justify-between px-4 py-6">
-            <ul class="flex items-center">
-                <li>
-                    <a href="#">as</a>
+            <ul class="flex items-center" > 
+                <li style="display: inline;padding:0px 10px 0px 10px;">
+                    <a href="#">Movie PlayBack 2R2R</a>
+                </li>
+                <li style="display: inline; padding:0px 10px 0px 10px;">
+                    <a href="#">Movies</a>
+                </li>
+                <li style="display: inline;padding:0px 10px 0px 10px;">
+                    <a href="#">TV Shows</a>
+                </li>
+                <li style="display: inline;padding:0px 10px 0px 10px;">
+                    <a href="#">Actors</a>
                 </li>
             </ul>
+            <div class="flex itemes-center">
+                <div class="relative">
+                    <input type="text" class="bg-gray-800 rounded-full w-64 px-4 py-1" placeholder="Search" style="display:inline;">
+                </div>
+            </div>
         </div>
     </nav>
     @yield('content')
