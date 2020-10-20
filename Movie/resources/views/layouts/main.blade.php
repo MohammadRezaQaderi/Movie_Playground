3f4bcbb53741a35bfb90440a2e8dd072c2934afa
MohammadRezaQaderi <you@example.com>
2020-10-20 13:05:41 +0330

    start again

---
+++ Movie/resources/views/layouts/main.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Movie App</title>
+    <link rel="stylesheet" href="/css/main.css">
+</head>
+<body class="font-sans bg-gray-900 text-white">
+    <nav class="border-b border-gray-800">
+        <div class="container mx-auto flex items-center justify-between px-4 py-6">
+            <ul class="flex items-center">
+                <li>
+                    <a href="#">as</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    @yield('content')
+</body>
+</html>
